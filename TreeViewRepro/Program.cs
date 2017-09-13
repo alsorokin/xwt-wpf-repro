1c666db62e918d3ef75c013133bbc10b6056ae40
Alexander Sorokin <alexander.sorokin@akvelon.com>
2017-09-13 14:14:42 +0300

    Created the repro app

---
+++ TreeViewRepro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xwt;
+
+class XwtDemo
+{
+    static int count;
+    [STAThread]
+    static void Main()
+    {
+        Application.Initialize(ToolkitType.Wpf);
+        var f = new DataField<string>();
+        TreeStore ts = new TreeStore(f);
+
+        var tree = new TreeView(ts);
+        tree.Columns.Add("MyColumn", f);
+        tree.HeadersVisible = false;
+        tree.MinHeight = 300;
+
+        Notebook nb = new Notebook();
+        nb.Add(new Label("Click the button below to add elements to the TreeView on the second tab. \nObserve that the result is different if you click it before changing tabs and after."), "One");
+        var vb = new VBox();
+        vb.PackStart(nb);
+
+        var addButton = new Button("Add element");
+        addButton.Clicked += (o, e) =>
+        {
+            var navigator = ts.AddNode().SetValue(f, "New element " + (++count).ToString());
+            tree.SelectRow(navigator.CurrentPosition);
+        };
+        vb.PackEnd(addButton);
+
+        nb.Add(tree, "Two");
+        var mainWindow = new Window()
+        {
+            Title = "Xwt TreeView Repro",
+            Width = 500,
+            Height = 500
+        };
+        mainWindow.Content = vb;
+        mainWindow.CloseRequested += (o, e) =>
+        {
+            Xwt.Application.Exit();
+        };
+        mainWindow.Show();
+        Application.Run();
+        mainWindow.Dispose();
+    }
+}
